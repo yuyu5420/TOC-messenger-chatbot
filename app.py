c01fe0056e591a55d8941ee2d961f27307c07d2a
yuyu <yuyu90267@gmail.com>
2018-12-19 08:49:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ PORT = os.environ['PORT']
 
 page = Page(ACCESS_TOKEN)
 machine = TocMachine();
-
 @route("/webhook", method="GET")
 def setup_webhook():
     mode = request.GET.get("hub.mode")
@@ -28,7 +27,12 @@ def setup_webhook():
 @route("/webhook", method="POST")
 def webhook_handler():
 	body = request.json
-	machine.state = "talking"
+	global Records
+	sender_id = event['sender']['id']
+	if Records[sender_id]:
+		machine.state = Records[sender_id]
+	else:
+		Records[sender_id] = machine.state
 	print('\nFSM STATE: ' + machine.state)
 	print('REQUEST BODY: ')
 	print(body)
@@ -39,9 +43,7 @@ def webhook_handler():
 		machine.advance(event)
 	# initial state
 	if machine.state == "init":
-		sender_id = event['sender']['id']
 		quick_replies = [QuickReply(title="休閒娛樂", payload="PICK_PLAY"), QuickReply(title="進食", payload="PICK_EAT"), QuickReply(title="睡覺", payload="PICK_SLEEP"), QuickReply(title="聊天", payload="PICK_TALK")]
-
 		page.send(sender_id, "嗨~\n你現在想要做什麼呢?",quick_replies=quick_replies,metadata="DEVELOPER_DEFINED_METADATA")
 
 	return 'OK'
